7734762b5ea27b60f412de0db5dc72ef4dc770da
Swearfy <swearfy@gmail.com>
2023-03-16 20:09:31 +0000

    added navbar button functionality

--- js/main.js
+++ js/main.js
@@ -1,13 +1,42 @@
+const navBar = document.getElementById("navBar");
+const body = document.querySelector("body");
+
 document.getElementById("navButton").addEventListener("click", function () {
-  document.getElementById("navBar").style.width = "100%";
-  document.querySelector("body").style.overflow = "hidden";
+  navBar.style.width = "100%";
+  body.style.overflow = "hidden";
 });
 
 document.getElementById("closeButton").addEventListener("click", function () {
-  document.getElementById("navBar").style.width = "0%";
-  document.querySelector("body").style.overflow = "auto";
+  navBar.style.width = "0%";
+  body.style.overflow = "auto";
 });
 
 document.getElementById("scrollDown").addEventListener("click", function () {
   document.getElementById("mySkills").scrollIntoView({ behavior: "smooth" });
+});
+
+document.getElementById("mainPage").addEventListener("click", function () {
+  document.getElementById("welcomePage").scrollIntoView({ behavior: "smooth" });
+  navBar.style.width = "0%";
+  body.style.overflow = "auto";
+});
+
+document.getElementById("skillPage").addEventListener("click", function () {
+  document.getElementById("skills").scrollIntoView({ behavior: "smooth" });
+  navBar.style.width = "0%";
+  body.style.overflow = "auto";
+});
+
+document.getElementById("projectPage").addEventListener("click", function () {
+  document.getElementById("projects").scrollIntoView({ behavior: "smooth" });
+  navBar.style.width = "0%";
+  body.style.overflow = "auto";
+});
+
+document.getElementById("contactPage").addEventListener("click", function () {
+  document
+    .getElementById("contactSection")
+    .scrollIntoView({ behavior: "smooth" });
+  navBar.style.width = "0%";
+  body.style.overflow = "auto";
 });

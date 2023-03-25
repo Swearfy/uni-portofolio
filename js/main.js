284c724ab15a2ba9104f77e60a8f803f0a3b6a20
Swearfy <swearfy@gmail.com>
2023-03-25 21:56:24 +0000

    readded button functionality

--- js/main.js
+++ js/main.js
@@ -2,12 +2,12 @@ const navBar = document.getElementById("navBar");
 const body = document.querySelector("body");
 const popup = document.getElementById("popup");
 
-document.getElementById("navButton").addEventListener("click", function () {
+document.getElementById("burgerButton").addEventListener("click", function () {
   navBar.style.width = "100%";
   body.style.overflow = "hidden";
 });
 
-document.getElementById("closeButton").addEventListener("click", function () {
+document.getElementById("burgerClose").addEventListener("click", function () {
   navBar.style.width = "0%";
   body.style.overflow = "auto";
 });
@@ -16,49 +16,51 @@ document.getElementById("scrollDown").addEventListener("click", function () {
   document.getElementById("skills").scrollIntoView({ behavior: "smooth" });
 });
 
-document.getElementById("contactme").addEventListener("click", function () {
+document.getElementById("contactMe-btn").addEventListener("click", function () {
   document
-    .getElementById("contactSection")
+    .getElementById("contact-Section")
     .scrollIntoView({ behavior: "smooth" });
 });
 
-document.getElementById("mainPage").addEventListener("click", function () {
-  document.getElementById("welcomePage").scrollIntoView({ behavior: "smooth" });
+document.getElementById("home-btn").addEventListener("click", function () {
+  document.getElementById("introPage").scrollIntoView({ behavior: "smooth" });
   navBar.style.width = "0%";
   body.style.overflow = "auto";
 });
 
-document.getElementById("skillPage").addEventListener("click", function () {
-  document.getElementById("skills").scrollIntoView({ behavior: "smooth" });
+document.getElementById("about-btn").addEventListener("click", function () {
+  document.getElementById("mySkills").scrollIntoView({ behavior: "smooth" });
   navBar.style.width = "0%";
   body.style.overflow = "auto";
 });
 
-document.getElementById("projectPage").addEventListener("click", function () {
+document.getElementById("projects-btn").addEventListener("click", function () {
   document.getElementById("projects").scrollIntoView({ behavior: "smooth" });
   navBar.style.width = "0%";
   body.style.overflow = "auto";
 });
 
-document.getElementById("contactPage").addEventListener("click", function () {
+document.getElementById("contact-btn").addEventListener("click", function () {
   document
-    .getElementById("contactSection")
+    .getElementById("contact-Section")
     .scrollIntoView({ behavior: "smooth" });
   navBar.style.width = "0%";
   body.style.overflow = "auto";
 });
 
-document.getElementById("gitHubProfile").addEventListener("click", function () {
-  window.open("https://github.com/Swearfy");
-});
+document
+  .getElementById("github-profile")
+  .addEventListener("click", function () {
+    window.open("https://github.com/Swearfy");
+  });
 
 document
-  .getElementById("linkedinProfile")
+  .getElementById("linkedin-profile")
   .addEventListener("click", function () {
     window.open("https://www.linkedin.com/in/dennispaulpuscasu/");
   });
 
-document.getElementById("emailLink").addEventListener("click", function () {
+document.getElementById("email-profile").addEventListener("click", function () {
   popup.classList.add("emailAnim");
   navigator.clipboard.writeText("swearfy@gmail.com");
   setTimeout(() => {

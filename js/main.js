1c49b137e0e6f07184b696085641f0d70d888cc5
Swearfy <swearfy@gmail.com>
2023-03-31 14:03:43 +0100

    Added fetch api to load projects from json file

--- js/main.js
+++ js/main.js
@@ -1,7 +1,12 @@
+fetch("/files/data.json")
+  .then((response) => response.json())
+  .then((data) => uploadProjects(data));
+
 const navBar = document.getElementById("navBar");
 const body = document.querySelector("body");
 const popup = document.getElementById("popup");
 const resumeContent = document.getElementById("colapse");
+const cardContainer = document.getElementById("container-cards");
 
 document.getElementById("burgerButton").addEventListener("click", function () {
   navBar.style.width = "100%";
@@ -78,14 +83,83 @@ document.getElementById("resume-btn").addEventListener("click", function () {
   }
 });
 
-document.getElementById("project1").addEventListener("click", function () {
-  window.open("https://github.com/Swearfy/Project-GameV2");
-});
+function uploadProjects(data) {
+  data.forEach((element) => {
+    const projectCard = createProject(element);
+    cardContainer.appendChild(projectCard);
+  });
+}
 
-document.getElementById("project2").addEventListener("click", function () {
-  window.open("https://github.com/Swearfy/SurveyApp");
-});
+function createProject(element) {
+  const projectCard = document.createElement("div");
+  projectCard.className = "project-card";
 
-document.getElementById("project3").addEventListener("click", function () {
-  window.open("https://github.com/Swearfy/Module_Chooser");
-});
+  // add image
+  const projectImage = document.createElement("img");
+  projectImage.className = "projectImage";
+  projectImage.alt = element.alt;
+  projectImage.src = element.img;
+
+  const projectDesc = createProjectDesc(element);
+
+  projectCard.appendChild(projectImage);
+  projectCard.appendChild(projectDesc);
+
+  return projectCard;
+}
+
+function createProjectDesc(element) {
+  //add description
+  const projectDesc = document.createElement("div");
+  projectDesc.className = "project-desc";
+
+  // description left side
+  const projectDescLeft = document.createElement("div");
+  projectDescLeft.className = "project-desc-left";
+
+  // title and description of project
+  const title = document.createElement("h2");
+  title.innerText = element.title;
+  const desc = document.createElement("p");
+  desc.innerText = element.description;
+
+  //description right side
+  const projectDescRight = document.createElement("div");
+  projectDescRight.className = "project-desc-right";
+
+  // project buttons github and live code
+  const githubButton = document.createElement("button");
+  githubButton.className = "github-button";
+  githubButton.addEventListener("click", function () {
+    window.open(element.url);
+  });
+
+  const buttonImg = document.createElement("img");
+  buttonImg.src = "/img/github.svg";
+  buttonImg.alt = "github logo";
+
+  githubButton.appendChild(buttonImg);
+
+  const liveCode = document.createElement("button");
+  liveCode.innerText = "Live Code";
+
+  // checks if live server url exist otherwise uses github url
+  liveCode.addEventListener("click", function () {
+    if (element.liveUrl === "") {
+      window.open(element.url);
+    } else {
+      window.open(element.liveUrl);
+    }
+  });
+
+  projectDescLeft.appendChild(title);
+  projectDescLeft.appendChild(desc);
+
+  projectDescRight.appendChild(githubButton);
+  projectDescRight.appendChild(liveCode);
+
+  projectDesc.appendChild(projectDescLeft);
+  projectDesc.appendChild(projectDescRight);
+
+  return projectDesc;
+}

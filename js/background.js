61a1693759759e0f4ab766bc0302b4ebeaa61fc0
Swearfy <swearfy@gmail.com>
2023-04-16 18:52:33 +0100

    moved canvas to background js

---
+++ js/background.js
@@ -0,0 +1,18 @@
+import { Effect } from "./effect.js";
+
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+let previouseTime;
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+const background = new Effect(canvas);
+
+export function animateBackground(currentTime) {
+  const delta = currentTime - previouseTime;
+  previouseTime = currentTime;
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  background.update(delta);
+  background.draw(ctx);
+  requestAnimationFrame(animateBackground);
+}

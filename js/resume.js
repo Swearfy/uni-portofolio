3f613a36d387790ed6ce19b914b85ed68aeee667
Swearfy <swearfy@gmail.com>
2023-04-16 20:05:10 +0100

    made resume loader as a modular function

--- js/resume.js
+++ js/resume.js
@@ -1,12 +1,14 @@
-// about me buttons and div
-const resumebutton = document.getElementById("resume-btn");
-const resumeContent = document.getElementById("colapse");
+export function loadResume() {
+  // about me buttons and div
+  const resumebutton = document.getElementById("resume-btn");
+  const resumeContent = document.getElementById("colapse");
 
-resumebutton.addEventListener("click", () => {
-  if (resumeContent.style.display === "block") {
-    resumeContent.style.display = "none";
-  } else {
-    resumeContent.style.display = "block";
-    document.getElementById("resume-btn");
-  }
-});
+  resumebutton.addEventListener("click", () => {
+    if (resumeContent.style.display === "block") {
+      resumeContent.style.display = "none";
+    } else {
+      resumeContent.style.display = "block";
+      document.getElementById("resume-btn");
+    }
+  });
+}
